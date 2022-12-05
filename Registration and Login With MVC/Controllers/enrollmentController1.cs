7b081fa7accdc1f272c9c20f5b063d5c9a04d3ee
nehaa <nehaa@DESKTOP-T520T9B>
2022-12-05 10:01:12 +0530

    Add project files.

---
+++ Registration and Login With MVC/Controllers/enrollmentController1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Registration_and_Login_With_MVC.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Registration_and_Login_With_MVC.Controllers
+{
+    public class enrollmentController1 : Controller
+    {
+        public string value = "";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Enroll e)
+        {
+            //if (HttpMethod == "POST")
+            {
+                Enroll er = new Enroll();
+                using (SqlConnection con = new SqlConnection("Data Source=PRIYANKA\\SQLEXPRESS;Integrated Security=true;Initial Catalog=Sample"))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SP_EnrollDetail", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@FirstName", e.FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", e.LastName);
+                        cmd.Parameters.AddWithValue("@Password", e.Password);
+                        cmd.Parameters.AddWithValue("@Gender", e.Gender);
+                        cmd.Parameters.AddWithValue("@Email", e.Email);
+                        cmd.Parameters.AddWithValue("@Phone", e.PhoneNumber);
+                        cmd.Parameters.AddWithValue("@SecurityAnwser", e.SecurityAnwser);
+                        cmd.Parameters.AddWithValue("@status", "INSERT");
+                        con.Open();
+                        ViewData["result"] = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Registration and Login With MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Registration_and_Login_With_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
